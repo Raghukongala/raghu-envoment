0e2d343651efff235e7ca77e5de81b8bed09456d
Raghukongala <kongalaraghu406@gmail.com>
2026-04-29 11:35:49 +0530

    Update vpc.tf

--- ecs-eac-project/vpc.tf
+++ ecs-eac-project/vpc.tf
@@ -1,21 +1,78 @@
+############################
+# VPC
+############################
 resource "aws_vpc" "main" {
- cidr_block = "10.0.0.0/16"
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "raghu-vpc"
+  }
 }
 
+############################
+# Public Subnet A
+############################
 resource "aws_subnet" "public_a" {
- vpc_id = aws_vpc.main.id
- cidr_block = "10.0.1.0/24"
- availability_zone = "ap-south-1a"
- map_public_ip_on_launch = true
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "ap-south-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet-a"
+  }
 }
 
+############################
+# Public Subnet B
+############################
 resource "aws_subnet" "public_b" {
- vpc_id = aws_vpc.main.id
- cidr_block = "10.0.2.0/24"
- availability_zone = "ap-south-1b"
- map_public_ip_on_launch = true
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "ap-south-1b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet-b"
+  }
 }
 
+############################
+# Internet Gateway
+############################
 resource "aws_internet_gateway" "igw" {
- vpc_id = aws_vpc.main.id
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "main-igw"
+  }
+}
+
+############################
+# Public Route Table
+############################
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "public-route-table"
+  }
+}
+
+############################
+# Associate Route Table
+############################
+resource "aws_route_table_association" "subnet_a_assoc" {
+  subnet_id      = aws_subnet.public_a.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_route_table_association" "subnet_b_assoc" {
+  subnet_id      = aws_subnet.public_b.id
+  route_table_id = aws_route_table.public_rt.id
 }
